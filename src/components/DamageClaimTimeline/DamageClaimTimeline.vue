293c93a232fd305eac523c7d3f14851915faeedf
Maxim Greiner <maximohneilian@users.noreply.github.com>
2025-05-06 13:27:31 +0200

    add DamageClaimTimelineItem to timeline

--- src/components/DamageClaimTimeline/DamageClaimTimeline.vue
+++ src/components/DamageClaimTimeline/DamageClaimTimeline.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { ProcessItem } from '@/lib/process-item.d.ts'
-import dayjs from 'dayjs'
+import DamageClaimeTimelineItem from './DamageClaimeTimelineItem.vue';
 
 const props = defineProps<{
   items: ProcessItem[]
@@ -10,7 +10,7 @@ const props = defineProps<{
 <template>
   <v-timeline align="start" side="end" size="default">
     <v-timeline-item v-for="(item, index) in props.items" :key="index" icon="mdi-check-network" fill-dot dot-color="green">
-      
+      <DamageClaimeTimelineItem :item="item"/>
     </v-timeline-item>
   </v-timeline>
 </template>
